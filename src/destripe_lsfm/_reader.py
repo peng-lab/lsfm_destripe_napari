1968ac97bd8e7b7416855d61e0318b08fcc19916
lennartkowitz <lennart.kowitz@isas.de>
2024-04-04 13:39:05 +0200

    implemented saving and loading of .tif/.tiff files

--- src/destripe_lsfm/_reader.py
+++ src/destripe_lsfm/_reader.py
@@ -5,7 +5,31 @@ It implements the Reader specification, but your plugin may choose to
 implement multiple readers or even other plugin contributions. see:
 https://napari.org/stable/plugins/guides.html?#readers
 """
-import numpy as np
+
+from pathlib import Path
+from qtpy.QtWidgets import QFileDialog
+from aicsimageio import AICSImage
+
+
+def open_dialog(parent):
+    """
+    Opens a dialog to select a file to open
+
+    Parameters
+    ----------
+    parent : QWidget
+        Parent widget for the dialog
+
+    Returns
+    -------
+    str
+        Path of the selected file
+    """
+    dialog = QFileDialog()
+    filepath, _ = dialog.getOpenFileName(
+        parent, "Select Image-File", filter="TIFF files (*.tif *.tiff)"
+    )
+    return filepath
 
 
 def napari_get_reader(path):
@@ -13,8 +37,8 @@ def napari_get_reader(path):
 
     Parameters
     ----------
-    path : str or list of str
-        Path to file, or list of paths.
+    path : str
+        Path to file
 
     Returns
     -------
@@ -22,51 +46,34 @@ def napari_get_reader(path):
         If the path is a recognized format, return a function that accepts the
         same path or list of paths, and returns a list of layer data tuples.
     """
-    if isinstance(path, list):
-        # reader plugins may be handed single path, or a list of paths.
-        # if it is a list, it is assumed to be an image stack...
-        # so we are only going to look at the first file.
-        path = path[0]
+    # if isinstance(path, list):
+    #     # reader plugins may be handed single path, or a list of paths.
+    #     # if it is a list, it is assumed to be an image stack...
+    #     # so we are only going to look at the first file.
+    #     path = path[0]
 
     # if we know we cannot read the file, we immediately return None.
-    if not path.endswith(".npy"):
+    if not (path.endswith(".tif") or path.endswith(".tiff")):
         return None
 
     # otherwise we return the *function* that can read ``path``.
-    return reader_function
+    return read_tiff
 
 
-def reader_function(path):
-    """Take a path or list of paths and return a list of LayerData tuples.
-
-    Readers are expected to return data as a list of tuples, where each tuple
-    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
-    both optional.
+def read_tiff(path):
+    """
+    Read a TIFF file
 
     Parameters
     ----------
-    path : str or list of str
-        Path to file, or list of paths.
+    path : str
+        Path to the file
 
     Returns
     -------
-    layer_data : list of tuples
-        A list of LayerData tuples where each tuple in the list contains
-        (data, metadata, layer_type), where data is a numpy array, metadata is
-        a dict of keyword arguments for the corresponding viewer.add_* method
-        in napari, and layer_type is a lower-case string naming the type of
-        layer. Both "meta", and "layer_type" are optional. napari will
-        default to layer_type=="image" if not provided
+    np.ndarray
+        Data read from the file
     """
-    # handle both a string and a list of strings
-    paths = [path] if isinstance(path, str) else path
-    # load all files into array
-    arrays = [np.load(_path) for _path in paths]
-    # stack arrays into single array
-    data = np.squeeze(np.stack(arrays))
-
-    # optional kwargs for the corresponding viewer.add_* method
-    add_kwargs = {}
-
-    layer_type = "image"  # optional, default is "image"
-    return [(data, add_kwargs, layer_type)]
+    filename = Path(path).name
+    data = AICSImage(path).get_image_data("YX")
+    return data, filename
